53f52ec02aacc582ec560939e4589170bb16507a
eric55123 <ken0970754487@gmail>
2025-01-03 15:28:05 +0800

    初始化 cakemall

---
+++ src/main/java/com/eric/cakemall/CakemallApplication.java
@@ -0,0 +1,13 @@
+package com.eric.cakemall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakemallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CakemallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eric/cakemall/CakemallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eric.cakemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakemallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
